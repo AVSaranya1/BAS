27b9ca48ab3cc5e2cce31de7702287dac35140d4
AndrewsChrispin <rajkumar.sathyamoorthy@intercorpsolutions.in>
2025-04-02 20:21:23 +0530

    Entity Menu

---
+++ DataAccessLayer/Interface/IEntityMenuDAL.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IEntityMenuDAL
+    {
+        Task<List<EntityMenuModel>> GetEntityMenu(string? strMode,string? struserGuid);
+        Task<List<MenusModel>> GetMenu(string UserNameGuid);
+    }
+}
