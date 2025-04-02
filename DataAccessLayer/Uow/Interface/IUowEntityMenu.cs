1dfac0207aee45bad0661d80fa66e10118b281f3
AndrewsChrispin <rajkumar.sathyamoorthy@intercorpsolutions.in>
2025-04-02 20:16:39 +0530

    Entity menu

---
+++ DataAccessLayer/Uow/Interface/IUowEntityMenu.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public  interface IUowEntityMenu : IDisposable
+    {
+        IEntityMenuDAL entityMenuRepo { get; }
+
+        void Commit();
+    }
+}
