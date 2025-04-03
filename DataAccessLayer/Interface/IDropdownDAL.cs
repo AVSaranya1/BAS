e693319525059afb3f9ac170e2a20d6cb2b2dc4f
[AVSaranya1] <saranya.veeraraghavan@intercorpsolutions.in>
2025-04-03 12:23:52 +0530

    Adding Dropdown Interface

---
+++ DataAccessLayer/Interface/IDropdownDAL.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Model;
+namespace DataAccessLayer.Interface;
+public interface IDropdownDAL
+{
+    Task<IEnumerable<DropDownModel>> getLevel(long userID);
+    Task<IEnumerable<DropDownModel>> getParentEntity(long UserID,string RefID1,string RefID2);
+    Task<IEnumerable<DropDownModel>> getCountry();
+    Task<IEnumerable<DropDownModel>> getCurrency();
+    Task<IEnumerable<TimezoneModel>> getTimeZone(long? RefID1);
+}
