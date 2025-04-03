2c1a8ab2b19125798e0ce049df88b4312043b806
[AVSaranya1] <saranya.veeraraghavan@intercorpsolutions.in>
2025-04-03 12:20:09 +0530

    IUowDropdown.cs

---
+++ DataAccessLayer/Uow/Interface/IUowDropdown.cs
@@ -0,0 +1,9 @@
+﻿using DataAccessLayer.Interface;
+
+namespace DataAccessLayer.Uow.Interface;
+public interface IUowDropdown:IDisposable
+{    
+        IDropdownDAL MasterDALRepo { get; }
+        void Commit();
+}
+
