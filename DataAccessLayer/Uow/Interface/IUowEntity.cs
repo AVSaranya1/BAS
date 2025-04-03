8339e8c6450ff963f70b68c29a2e0a7c9f79d9a9
[AVSaranya1] <saranya.veeraraghavan@intercorpsolutions.in>
2025-04-03 12:19:37 +0530

    IUowEntity.cs

---
+++ DataAccessLayer/Uow/Interface/IUowEntity.cs
@@ -0,0 +1,9 @@
+﻿using DataAccessLayer.Interface;
+
+namespace DataAccessLayer.Uow.Interface;
+
+public interface IUowEntity: IDisposable
+{
+    IEntityDAL EntityDALRepo { get; }
+    void Commit();
+}
