c633493860516c08b2566bcc52295eb060fde01c
[AVSaranya1] <saranya.veeraraghavan@intercorpsolutions.in>
2025-04-03 12:23:17 +0530

    Adding Entity Interface

---
+++ DataAccessLayer/Interface/IEntityDAL.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Model;
+using System.Data;
+
+namespace DataAccessLayer.Interface;
+
+public interface IEntityDAL
+{
+    Task<List<EntityModel>> GetAllLevelDetail();
+    Task<EntityModel> GetLevelDetailByGuid(string Guid);    
+    Task<bool> InsertLevelDetailAsync(EntityModel model);
+    Task<bool> UpdateLevelDetailAsync(EntityModel model);
+    Task<(bool deleteLevelDetail, List<DeleteLevelDetailResult> deleteResults)> DeleteLevelDetailAsync(DataTable deleteLevelDetailTable);
+}
