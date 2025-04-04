155567352b29d9b6479c6e2a436169a57124d5ea
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-04 20:11:06 +0530

    Uow Client User

---
+++ DataAccessLayer/Uow/Interface/IUowClientUser.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Interface;
+using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowClientUser : IDisposable
+    {
+        IClientUserAccountDAL UserAccountDALRepo { get; }
+        void Commit();
+    }
+}
