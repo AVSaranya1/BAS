e91184b9a5d145d7074d5f5e6b24abc84fba371e
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-04 20:04:45 +0530

    Added Logic for Client Level User Creation

--- DataAccessLayer/Interface/IGUIDDAL.cs
+++ DataAccessLayer/Interface/IGUIDDAL.cs
@@ -1,6 +1,4 @@
-﻿
-
-using DataAccessLayer.Implementation;
+﻿using DataAccessLayer.Implementation;
 using DataAccessLayer.Services;
 
 namespace DataAccessLayer.Interface
@@ -8,13 +6,16 @@ namespace DataAccessLayer.Interface
     public interface IGUIDDAL
     {
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUID(string? UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetClientGUID(string? UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserAccountRoleGuid(string UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnRoleGuid(string UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnOrgName(string UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserPolicy(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnClientUserPolicy(string? UserGuid,string? ClientDBName);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnNationality(string UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnMailServer(string UserGuid);
-
-
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnClientRoleGuid(string UserGuid, string? ClientDBName);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnClientLevelInfo(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnClientDivision(string? updatedGuidBy);
     }
 }
