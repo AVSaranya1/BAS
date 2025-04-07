bb99f538b77c2f1f26e610738fb4300c90e8c879
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-07 18:20:39 +0530

    Added Password Reset

--- DataAccessLayer/Implementation/ForgotPasswordDAL.cs
+++ DataAccessLayer/Implementation/ForgotPasswordDAL.cs
@@ -15,19 +15,19 @@ namespace DataAccessLayer.Implementation
             
         }
         
-        public async Task<(List<ForgotPasswordModel> forgotPasswordModels, int RetVal, string Msg)> InsertUpdateForgotPassword(ForgotPasswordModel model)
+        public async Task<(List<ForgotPasswordModel> forgotPasswordModels, int RetVal, string Msg)> InsertUpdateForgotPassword(ForgotPasswordModel model, string Token)
         {
             DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("@UserName", model.UserName);
-            parameters.Add("@Token", model.Token);
+            parameters.Add("@EmailID", model.EmailID);
+            parameters.Add("@Token", Token);
              parameters.Add("@Mode", Common.PageMode.ADD);
             parameters.Add("@RetVal", dbType: DbType.Int32, direction: ParameterDirection.Output);
             parameters.Add("@RetMsg", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
-            using (var result = await Connection.QueryMultipleAsync("sp_ForgotPassword",
+            using var result = await Connection.QueryMultipleAsync("sp_ForgotPassword",
                                                                         parameters,
                                                                         transaction: Transaction,
-                                                                        commandType: CommandType.StoredProcedure))
-            {
+                                                                        commandType: CommandType.StoredProcedure);
+            
                 // Process the first result set (roles)
                 var forgotPasswordModels = result.Read<ForgotPasswordModel>().ToList();
                 // Ensure all result sets are consumed to retrieve the output parameters
@@ -42,7 +42,7 @@ namespace DataAccessLayer.Implementation
 
                 // Return the roles list along with output parameters
                 return (forgotPasswordModels, retVal, msg);
-            }
+            
         }
         public MailServer mailServerPort()
         {
@@ -53,7 +53,7 @@ namespace DataAccessLayer.Implementation
             DynamicParameters parameters = new DynamicParameters();
             
             parameters.Add("@Token", token);
-            parameters.Add("@Mode", "VALIDATE_TOKEN");
+            parameters.Add("@Mode", Common.PageMode.VALIDATE_TOKEN);
             
             using (var result = await Connection.QueryMultipleAsync("sp_ForgotPassword",
                                                                         parameters,
@@ -62,6 +62,36 @@ namespace DataAccessLayer.Implementation
             {
                 return result.Read<GetForgotPasswordModel?>().FirstOrDefault();
             }
+        }
+        public async Task<(ForgotPassword forgotPasswordModels, int RetVal, string Msg)> ResetPassword(ForgotPassword LM)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+
+            parameters.Add("@UserName", LM.UserName);
+            parameters.Add("@Mode", Common.PageMode.RESET_PWD_MASTER);
+            parameters.Add("@Password", LM.Password);
+            parameters.Add("@RetVal", DbType.Int64, direction: ParameterDirection.Output);
+            parameters.Add("@RetMsg", DbType.String, direction: ParameterDirection.Output);
+
+            using var result = await Connection.QueryMultipleAsync("sp_ForgotPassword",
+                                                                        parameters,
+                                                                        transaction: Transaction,
+                                                                        commandType: CommandType.StoredProcedure);
+
+            var forgotPasswordModels = result.Read<ForgotPassword>().FirstOrDefault();
+            // Ensure all result sets are consumed to retrieve the output parameters
+            while (!result.IsConsumed)
+            {
+                result.Read(); // Process remaining datasets
+            }
+
+            // Access the output parameters after consuming all datasets
+            int retVal = parameters.Get<int?>("@RetVal") ?? -4;
+            string msg = parameters.Get<string?>("@RetMsg") ?? "No Records Found";
+
+            // Return the roles list along with output parameters
+            return (forgotPasswordModels, retVal, msg);
+
         }
     }
 }
