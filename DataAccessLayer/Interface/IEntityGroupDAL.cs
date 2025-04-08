2e8bdf25cca82a73f676307901cd64aff369693c
AndrewsChrispin <rajkumar.sathyamoorthy@intercorpsolutions.in>
2025-04-08 22:59:41 +0530

    Entity Group

---
+++ DataAccessLayer/Interface/IEntityGroupDAL.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IEntityGroupDAL
+    {
+      Task<List<EntityGroupModel>> GetEntityGroup(EntityGroupModel entityGroupModel);
+      Task<List<EntityGroupModel>> GetEntityGroupDetails(EntityGroupModel entityGroupModel);
+
+        Task<string> AddEntityGroup(EntityGroupModel entityGroupModel);
+        Task<string> EditEntityGroup(EntityGroupModel entityGroupModel);
+
+        Task<List<DeleteResultModel>> DeleteEntityGroup(List<EntityGroupDel> lstEntityGroupDel, string strMode,string strUserGuid);
+    }
+}
