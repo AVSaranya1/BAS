499d26aba1bb4549eb7d12bb3392e10040c741ac
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-08 13:50:06 +0530

    Added Guid Column

--- DataAccessLayer/Interface/IForgotPasswordDAL.cs
+++ DataAccessLayer/Interface/IForgotPasswordDAL.cs
@@ -6,7 +6,7 @@ namespace DataAccessLayer.Interface
     public interface IForgotPasswordDAL
     {
         Task<(List<ForgotPasswordModel> forgotPasswordModels, int RetVal, string Msg)> InsertUpdateForgotPassword(ForgotPasswordModel LM, string Token);
-        Task<(ForgotPassword forgotPasswordModels, int RetVal, string Msg)> ResetPassword(ForgotPassword LM);
+        
         MailServer mailServerPort();
         Task<GetForgotPasswordModel?> ValidateTokenForgotPassword(string? token);
     }
