73609a0bb1c98c1c8ec7ba92bf006684837ff648
AndrewsChrispin <rajkumar.sathyamoorthy@intercorpsolutions.in>
2025-04-08 23:03:06 +0530

    Entity Group

---
+++ DataAccessLayer/Uow/Interface/IUowEntityGroup.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowEntityGroup: IDisposable
+    {
+        IEntityGroupDAL entityGroupRepo { get; }
+        void Commit();
+    }
+}
