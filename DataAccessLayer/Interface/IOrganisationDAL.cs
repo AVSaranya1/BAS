712e1ade11b3ea73352b2da8a08313ee9cc900ad
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-10 15:53:51 +0530

    Added Industry ID in Organisation

--- DataAccessLayer/Interface/IOrganisationDAL.cs
+++ DataAccessLayer/Interface/IOrganisationDAL.cs
@@ -19,6 +19,7 @@ namespace DataAccessLayer.Interface
 
         Task<List<OrganisationDeleteRecord>> DeleteOrganisation(List<DeleteRecord> dltOrg);
         Task<List<DataLocationDropdown>> DataLocationInDropdown();
+        Task<List<DropDownModel>> IndustryDropdown();
         Task<List<OrganisationModules>> GetAllModules();
     }
 }
