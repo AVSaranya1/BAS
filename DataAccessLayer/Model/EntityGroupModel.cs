ae7e8f9e38786cf1ae981d5dc503e720edf354b7
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-10 22:20:18 +0530

    Change the value

--- DataAccessLayer/Model/EntityGroupModel.cs
+++ DataAccessLayer/Model/EntityGroupModel.cs
@@ -13,7 +13,7 @@ namespace DataAccessLayer.Model
         public string? Mode { get; set; }
         public long? ID { get; set; }
         public long? ParentID { get; set; }
-        public string? Guid { get; set; }
+        public Guid? Guid { get; set; }
         public string? EntityGroupCode { get; set; }
         public string? EntityGroupDesc { get; set; }
         public string? Logo { get; set; }
@@ -33,6 +33,6 @@ namespace DataAccessLayer.Model
     public class EntityGroupDel
     {
         public long? ID { get; set; }
-        public string? Guid { get; set; }
+        public Guid? Guid { get; set; }
     }
 }
