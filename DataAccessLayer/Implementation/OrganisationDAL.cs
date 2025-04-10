1c5f6f40a5a0235bbf3f8b00ef95ca31852253f9
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-10 20:26:56 +0530

    Added DataLocationId

--- DataAccessLayer/Implementation/OrganisationDAL.cs
+++ DataAccessLayer/Implementation/OrganisationDAL.cs
@@ -56,6 +56,7 @@ namespace DataAccessLayer.Implementation
                     parameters.Add("@Active", model.Active);
                     parameters.Add("@IndustryID", model.IndustryID);
                     parameters.Add("@Mode", Common.PageMode.ADD);
+                    parameters.Add("@DataLocationID", model.DataLocationID);
                     parameters.Add("@Msg", dbType: DbType.String, size: 2000, direction: ParameterDirection.Output);
 
                     await connection.ExecuteAsync(
@@ -202,6 +203,7 @@ namespace DataAccessLayer.Implementation
                     parameters.Add("@Active", model.Active);
                     parameters.Add("@Guid", model.Guid);
                     parameters.Add("@IndustryID", model.IndustryID);
+                    parameters.Add("@DataLocationID", model.DataLocationID);
                     parameters.Add("@Mode", Common.PageMode.EDIT);
                     parameters.Add("@PinCode", model.PinCode);
                     parameters.Add("@Msg", dbType: DbType.String, size: 2000, direction: ParameterDirection.Output);
