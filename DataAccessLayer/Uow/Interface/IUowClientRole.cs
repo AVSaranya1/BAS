d47c1bce5728f5fcb03c4b1d7bc92f97001cc3e3
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-15 17:08:28 +0530

    Added Client Role

---
+++ DataAccessLayer/Uow/Interface/IUowClientRole.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowClientRole : IDisposable
+    {
+        IClientRoleDAL RoleDALRepo { get; }
+        void Commit();
+    }
+}
