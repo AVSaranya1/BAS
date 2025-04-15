063aab1979237c0ee60c31330930c475711876f8
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-15 16:47:30 +0530

    Added User Policy

---
+++ DataAccessLayer/Interface/IClientUserGroupDAL.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Model;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IClientUserGroupDAL
+    {
+        Task<List<GetClientUserGroupModel?>> GetAllUserPolicy(long UpdatedBy, string ClientDBName);
+        Task<(bool InsertUserGroup, int RetVal, string Msg)> InsertUpdateUserPolicy(ClientUserGroupModel? UM);
+
+        Task<(bool deleteuserGroup, List<DeleteClientUserGroupResult> deleteResults)> DeleteUserPolicy(long UpdatedBy, DeleteClientUserGroup deleteUserGroup);
+        Task<GetClientUserGroupModel?> GetUserPolicyByGUId(string? GUId,string? ClientDBName);
+        Task<(bool UpdateUserGroup, int RetVal, string Msg)> UpdateUserPolicyAsync(ClientUpdateUserGroupModel? userAccount);
+    }
+}
