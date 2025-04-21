456b0ceede4b81181e8e9309ffa813e3fc5ae75d
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-21 16:21:22 +0530

    Issue Fixed for Conversion of Guid to String

--- DataAccessLayer/Interface/IDivisionDAL.cs
+++ DataAccessLayer/Interface/IDivisionDAL.cs
@@ -8,7 +8,7 @@ namespace DataAccessLayer.Interface
         Task<(bool InsertClientDivision, long RetVal, string Msg)> InsertUpdateClientDivision(DivisionModel NM);
         Task<(bool UpdateClientDivision, long RetVal, string Msg)> UpdateClientDivision(UpdateDivision NM);
         Task<(bool deleteClientDivision, List<DeleteDivisionResult> deleteResults)> DeleteClientDivision(long Id, DeleteDivision deleteClientDivision);
-        Task<(GetDivisionModel? getClientDivisionModel,  List<DropDownModel?> getBusinessEntityDatatables, List<DropDownModel?> getCostCenterDivisionDatatables, List<DropDownModel?> getDepartmentDatatables)> GetClientDivisionByGUId(string GuId);
+        Task<(GetDivisionModel? getClientDivisionModel,  List<DropDownModel?> getBusinessEntityDatatables, List<DropDownModel?> getCostCenterDivisionDatatables, List<MultiSelectionDropDownModel?> getDepartmentDatatables)> GetClientDivisionByGUId(string GuId);
         Task<(List<DropDownModel>? getBusinessEntityDatatables, List<DropDownModel?> getCostCenterDivisionDatatables, List<DropDownModel?> getDepartmentDatatables)> GetClientDivisionDeptCatMap();
     }
     
