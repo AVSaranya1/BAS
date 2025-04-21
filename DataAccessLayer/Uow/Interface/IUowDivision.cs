c1228a669a0e5dd1d8318229a4d5f12c5c9c4264
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-21 10:17:25 +0530

    Done as per the Functionality Document

---
+++ DataAccessLayer/Uow/Interface/IUowDivision.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Interface;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowDivision : IDisposable
+    {
+        IDivisionDAL ClientDivisionDALRepo { get; }
+        void Commit();
+    }
+}
