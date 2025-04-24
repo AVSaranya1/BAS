55d4e41d1ed651b79063c8630d084f5cce28b19c
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-24 08:15:48 +0530

    Added Cost Center Files

---
+++ DataAccessLayer/Uow/Implementation/UowCostCenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DataAccessLayer.Implementation;
+using DataAccessLayer.Interface;
+using DataAccessLayer.Uow.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataAccessLayer.Uow.Implementation
+{
+    public class UowCostCenter: UowCommon, IUowCostCenter
+    {
+        public UowCostCenter(IHttpContextAccessor httpContextAccessor)
+            : base(httpContextAccessor)
+        {
+        }
+        public ICostCenterDAL CostCenterDALRepo
+        {
+            get
+            {
+                return new CostCenterDAL(Transaction, GetConnectionStringFromContext());
+            }
+        }
+        
+    }
+}
