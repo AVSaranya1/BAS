81b487065ae52822532c8a7c914e08ce5c9c636e
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-04-24 08:19:12 +0530

    Added Cost Center Files

---
+++ DataAccessLayer/Interface/ICostCenterDAL.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Model;
+
+using System.Data;
+
+namespace DataAccessLayer.Interface
+{
+    public interface ICostCenterDAL
+    {
+        Task<IEnumerable<GetCostCenterModel>> GetAllCostCenter();
+        Task<(GetCostCenterModel getCostCenterModel, List<MultiSelectionDropDownModel> getBusinessEntityTables, List<MultiSelectionDropDownModel>? getDivisionDatatables, List<MultiSelectionDropDownModel?> getDepartmentDatatables)> GetCostCenterByGuId(string GuId);
+        Task<string> AddCostCenterAsync(CostCenterModel model, DataTable dataTable);
+        Task<string> UpdateCostCenterAsync(UpdateCostCenterModel model, DataTable dataTable);
+        Task<string> DeleteCostCenterAsync(DataTable deleteLevelDetailTable, string? ModifiedBy);
+        Task<IEnumerable<DropDownModel>> getMapParentCostCenter();
+        Task<(List<DropDownModel?> getBusinessEntityTables, List<DropDownModel?> getDivisionDatatables, List<DropDownModel?> getDepartmentDatatables)> getMapBUDivisionDept();
+
+
+    }
+}
